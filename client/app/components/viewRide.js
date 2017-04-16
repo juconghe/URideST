0cd25e961d25948bd9590f362eb78558b63cd981
Jucong He <hejucong1995@gmail.com>
2017-04-16 15:49:02 -0400

    move file back to the folder

---
+++ client/app/components/viewRide.js
@@ -0,0 +1,12 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+import Navbar from './components/navbar';
+import RideTable from './components/rideTable'
+
+export default class ViewRide extends React.Component {
+  render() {
+    return (
+      <Navbar />
+    );
+  }
+}
