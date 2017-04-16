fac3f9f2171b814c26b45353739541d5ef5bd528
Jucong He <hejucong1995@gmail.com>
2017-04-16 15:16:21 -0400

    put singleton class and router

--- client/app/app.js
+++ client/app/app.js
@@ -7,6 +7,14 @@ const ReactDOM = require('react-dom');
 class App extends React.Component{
   render() {
     return (
+      <Client />
+    );
+  }
+}
+
+class Client extends React.Component{
+  render() {
+    return (
       <div>
         <Navbar />
         <RequestBtns />
@@ -22,7 +30,54 @@ class App extends React.Component{
   }
 }
 
+class ViewRide extends React.Component {
+
+}
+
+class PickUp extends React.Component{
+
+}
+
+class DropOff extends React.Component {
+
+}
+
+class Calendar extends React.Component {
+
+}
+
+class AvailableTime extends React.Component {
+
+}
+
+class RequestDate extends React.Component {
+
+}
+
+class Finished extends React.Component {
+
+}
+
+class SpecialAccess extends React.Component {
+
+}
+
+class Dispatcher extends React.Component {
+
+}
+
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+        <IndexRoute component={Client}/>
+        <Route path="/calendar" component={Calendar}/>
+        <Route path="/pickUp" component={PickUp}/>
+        <Route path="/dropOff" component={DropOff}/>
+        <Route path="/specialAccess" component={SpecialAccess}/>
+        <Route path="/availableTime" component={AvailableTime}/>
+        <Route path="/finished" component={Finished}/>
+        <Route path="/viewRide" component={ViewRide}/>
+    </Route>
+  </Router>,
   document.getElementById('App')
 );
