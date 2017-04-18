dc0f675a48a781d684ea8ea50f81fc6b68f6aa00
timmliew <liew_timothy@Tims-MacBook-Pro.local>
2017-04-17 20:25:20 -0400

    deleted extra back button

--- client/app/components/footer.js
+++ client/app/components/footer.js
@@ -82,9 +82,6 @@ class Footer extends React.Component {
               onClick={this.handleNextPage}>
               Submit
             </a>
-            <a ref="#" type="button"
-              className="btn btn-default"
-              onClick={this.handlePrevPage}>Back</a>          
             <a ref="/" type="button"
               className="btn btn-default"
               onClick={this.handlePrevPage}>Back</a>
