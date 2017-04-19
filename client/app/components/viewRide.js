edb099c9407187b12109fbae65e0ad54af1c8bba
jeremy <oxonwater@hotmail.com>
2017-04-19 17:57:18 -0400

    fixed splitting viewRide

--- client/app/components/viewRide.js
+++ client/app/components/viewRide.js
@@ -1,42 +1,42 @@
 const React = require('react');
 const RideTable = require('./rideTable');
 import {getPreviousRideData} from '../server';
-import {getPendingRideData} from '../server';
 import {getConfirmedRideData} from '../server';
+import {getPendingRideData} from '../server';
 
 class ViewRide extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      previosRideArray:[{
-        _id:null,
-        pickupTime:null,
-        pickupDate:null,
-        isConfirmed:null,
-        dropoff:null,
-        pickup:null,
-        van:null,
-        user:null
-      }]
-      pendingRideArray:[{
-        _id:null,
-        pickupTime:null,
-        pickupDate:null,
-        isConfirmed:null,
-        dropoff:null,
-        pickup:null,
-        van:null,
-        user:null
-      }]
-      confirmedRideArray:[{
-        _id:null,
-        pickupTime:null,
-        pickupDate:null,
-        isConfirmed:null,
-        dropoff:null,
-        pickup:null,
-        van:null,
-        user:null
+      previousRideArray:[{
+      _id:null,
+      pickupTime:null,
+      pickupDate:null,
+      isConfirmed:null,
+      dropoff:null,
+      pickup:null,
+      van:null,
+      user:null
+    }],
+    ConfirmedRideArray:[{
+      _id:null,
+      pickupTime:null,
+      pickupDate:null,
+      isConfirmed:null,
+      dropoff:null,
+      pickup:null,
+      van:null,
+      user:null
+    }],
+    PendingRideArray:[{
+      _id:null,
+      pickupTime:null,
+      pickupDate:null,
+      isConfirmed:null,
+      dropoff:null,
+      pickup:null,
+      van:null,
+      user:null
     }]
     }
   }
@@ -45,24 +45,23 @@ class ViewRide extends React.Component {
       console.log(data);
       this.setState({previousRideArray:data});
     });
+    getConfirmedRideData(1,(data)=> {
+      console.log(data);
+      this.setState({ConfirmedRideArray:data});
+    });
     getPendingRideData(1,(data)=> {
       console.log(data);
       this.setState({pendingRideArray:data});
     });
-    getConfirmedRideData(1,(data)=> {
-      console.log(data);
-      this.setState({confirmedRideArray:data});
-    });
-
   }
 
   render() {
     let rideData = this.state.rideArray;
     return (
       <div>
-      <RideTable title="Previous Ride" rideData={previousRideData}/>
-      <RideTable title="Pending Ride" rideData={pendingRideData}/>
-      <RideTable title="Confirmed Ride" rideData={confirmedRideData}/>
+      <RideTable title="Previous Ride" rideData={rideData}/>
+      <RideTable title="Pending Ride" rideData={rideData}/>
+      <RideTable title="Confirmed Ride" rideData={rideData}/>
       </div>
   );
   }
