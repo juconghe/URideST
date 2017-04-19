a6670f80109aa03be71dbef3e607243e60066a9c
jeremy <oxonwater@hotmail.com>
2017-04-19 14:32:00 -0400

    replacing vewRide with previous, pending, cofirmed

--- client/app/components/viewRide.js
+++ client/app/components/viewRide.js
@@ -1,39 +1,68 @@
 const React = require('react');
 const RideTable = require('./rideTable');
 import {getPreviousRideData} from '../server';
-import {getConfirmedRideData} from '../server';
 import {getPendingRideData} from '../server';
+import {getConfirmedRideData} from '../server';
 
 class ViewRide extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rideArray:[{
-      _id:null,
-      pickupTime:null,
-      pickupDate:null,
-      isConfirmed:null,
-      dropoff:null,
-      pickup:null,
-      van:null,
-      user:null
+      previosRideArray:[{
+        _id:null,
+        pickupTime:null,
+        pickupDate:null,
+        isConfirmed:null,
+        dropoff:null,
+        pickup:null,
+        van:null,
+        user:null
       }]
+      pendingRideArray:[{
+        _id:null,
+        pickupTime:null,
+        pickupDate:null,
+        isConfirmed:null,
+        dropoff:null,
+        pickup:null,
+        van:null,
+        user:null
+      }]
+      confirmedRideArray:[{
+        _id:null,
+        pickupTime:null,
+        pickupDate:null,
+        isConfirmed:null,
+        dropoff:null,
+        pickup:null,
+        van:null,
+        user:null
+    }]
     }
   }
   componentWillMount() {
-    getRideData(1,(data)=> {
+    getPreviousRideData(1,(data)=> {
       console.log(data);
-      this.setState({rideArray:data});
+      this.setState({previousRideArray:data});
     });
+    getPendingRideData(1,(data)=> {
+      console.log(data);
+      this.setState({pendingRideArray:data});
+    });
+    getConfirmedRideData(1,(data)=> {
+      console.log(data);
+      this.setState({confirmedRideArray:data});
+    });
+
   }
 
   render() {
     let rideData = this.state.rideArray;
     return (
       <div>
-      <RideTable title="Previous Ride" rideData={rideData}/>
-      <RideTable title="Pending Ride" rideData={rideData}/>
-      <RideTable title="Confirmed Ride" rideData={rideData}/>
+      <RideTable title="Previous Ride" rideData={previousRideData}/>
+      <RideTable title="Pending Ride" rideData={pendingRideData}/>
+      <RideTable title="Confirmed Ride" rideData={confirmedRideData}/>
       </div>
   );
   }
