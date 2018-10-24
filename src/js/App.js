6b8ea5fea80e1f2dca40d057c551a4ef5063c6bd
Jucong <hejucong1995@gmail.com>
2018-10-23 21:36:01 -0400

    created buttons

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Navbar from './component/Navbar';
-
+import SelectionButtons from './component/SelectionButtons'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <SelectionButtons />
       </div>
     );
   }
---
+++ src/js/component/SelectionButtons.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  demo: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit * 50,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  button: {
+    padding: theme.spacing.unit * 2,
+    height: '100%',
+    color: theme.palette.text.secondary,
+    width: '200px',
+  },
+});
+
+class SelectionButtons extends Component {
+  render() {
+    const {classes} = this.props;
+    return (
+      <Grid container className={classes.root}>
+        <Grid item xs={12}>
+          <Grid 
+            container
+            spacing={16}
+            className={classes.demo}
+            alignItems="center"
+            direction="column"
+            justify="center"
+          >
+            <Grid item>
+              <Button variant="contained" className={classes.button}>
+                Request A Ride
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" className={classes.button}>
+                View Rides
+              </Button>
+          </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+SelectionButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+export default withStyles(styles)(SelectionButtons);
